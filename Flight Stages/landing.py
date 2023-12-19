2410200a433145884b89c5afea379a37cf6e9438
nawpy <116829245+nawpy@users.noreply.github.com>
2023-12-19 23:13:37 +0300

    Added Landing Stage

---
+++ Flight Stages/landing.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+m0 = 58000
+m = 1208
+P = 68000
+U = 1400
+
+t = [i for i in range(1, 18, 2)]
+
+g = 1.63
+k = 330
+fuel_values = list()
+
+for i in range(len(t)):
+    wy = P / (m0 - k * t[i])
+    V =  U - wy * t[i]
+    M = m - P / (V / t[i] - g)
+    fuel_values.append(M)
+
+
+plt.plot(t, fuel_values)
+plt.title('Зависимость количества топлива от времени')
+plt.xlabel('Время, мин')
+plt.ylabel('Количество топлива, кг')
+plt.grid(True)
+plt.show()
