61f3b83947947e771f76c79313986b5a0d0fa616
matv3yka <116829245+matv3ykaa@users.noreply.github.com>
2023-12-24 20:18:41 +0300

    Updated transition.py

--- Flight Stages/transition.py
+++ Flight Stages/transition.py
@@ -1,38 +1,41 @@
 import matplotlib.pyplot as plt
 
-m = 2155
-u2 = 2630
-u1 = 1400
-r1 = 4700000
+m = 273000
+u1 = 9258
+u2 = 543
+r1 = 46738
 r2 = 42164000
 r = r2 / r1
+I = 2796
+g = 9.81
 
-V1 = u1 * (1 - (2 / (r + 1))**0.5)
-V2 = u2 * (((2 * r) / (r + 1))**0.5 - 1)
+V1 = u1 * ((2 * r / (r + 1))**0.5 - 1)
+V2 = u2 * (1 - (2 / (r + 1))**0.5)
 P = 65500
+e = (r2 - r1) / (r2 + r1)
 
-t1 = [i for i in range(1, 11, 5)]
-t2 = [i for i in range(1, 11, 5)]
-t = [i for i in range(1, 36, 5)]
+t = [i for i in range(1, 213)]
 
 fuel_values = list()
+M = 2155
+M1 = round(m * (1 - (e**(-((V1) / I)))))
+M2 = round(m * (1 - (e**(-((V2) / I)))))
+for i in range(1, abs(M1) + 1, 100):
+    fuel_values.append(M - i)
 
-for i in range(len(t1)):
-    M = m - ((P * t1[i]) / V1)
-    fuel_values.append(M)
+curr_M = fuel_values[-1]
 
-m = M
+for r in range(200):
+    fuel_values.append(curr_M)
 
-for r in range(3):
-  fuel_values.append(M)
+for i in range(1, abs(M2) + 1, 50):
+    fuel_values.append(curr_M - i)
 
-for i in range(len(t2)):
-    M = m - ((P * t2[i]) / V2)
-    fuel_values.append(M)
 
+print(fuel_values, M1, M2)
 plt.plot(t, fuel_values)
 plt.title('Зависимость количества топлива от времени')
-plt.xlabel('Время, сек')
+plt.xlabel('Время, мин')
 plt.ylabel('Количество топлива, кг')
 plt.grid(True)
 plt.yticks(plt.yticks()[0], [f'{int(y)}' for y in plt.yticks()[0]])
