29587d6aa37dddc1c23afe5be0cc83ea8a0989a3
matv3yka <116829245+matv3ykaa@users.noreply.github.com>
2023-12-26 13:53:31 +0300

    Updated landing.py

--- Flight Stages/landing.py
+++ Flight Stages/landing.py
@@ -4,12 +4,12 @@ m0 = 58000
 m = 1208
 P = 68000
 U = 1400
+g = 1.63
+k = 330
 
 t = [i for i in range(1, 18, 2)]
 t1 = [1, 30, 60, 90, 120, 150, 180, 210, 240]
 
-g = 1.63
-k = 330
 fuel_values = list()
 
 for i in range(len(t)):
